a0e56fb694731d00f04a929970f41979d79f33e0
Alisson Boeing <alisson.b11@aluno.ifsc.edu.br>
2019-10-04 17:30:15 -0300

    arrumar demux

--- Projeto1/A2_2019_2_restored/top_timer_de2_115.vhd
+++ Projeto1/A2_2019_2_restored/top_timer_de2_115.vhd
@@ -57,10 +57,19 @@ architecture top_a3_2019_2 of top_timer_de2_115 is
 	 signal en1hz: std_logic;
 	 signal en360hz: std_logic;
 	 
-	 --signal ctrl: std_logic_vector(2 downto 0);
+	 signal ctrl_next: unsigned(2 downto 0);
 	 signal ctrl: unsigned(2 downto 0);
 	 signal tobcd : std_logic_vector(3 downto 0);
-		
+	 signal dout	:  std_logic_vector ((7*6)-1 downto 0);
+    signal dout0	:  std_logic_vector (6 downto 0);
+	 signal dout1	:  std_logic_vector (6 downto 0);
+	 signal dout2	:  std_logic_vector (6 downto 0);
+	 signal dout3	:  std_logic_vector (6 downto 0);
+	 signal dout4	:  std_logic_vector (6 downto 0);
+	 signal dout5	:  std_logic_vector (6 downto 0);
+	 signal dout6	:  std_logic_vector (6 downto 0);
+	 signal saidaconversor : std_logic_vector (6 downto 0);
+	 
 begin
 
 	reset <= not KEY(0);
@@ -76,6 +85,7 @@ begin
 	 elsif (CLOCK_10khz'event and CLOCK_10khz='1') then
 	    r_reg <= r_next;
 		 r_reg360 <= r_next360;
+		 ctrl <= ctrl_next;
 	 end if;
 	end process;
 	
@@ -91,28 +101,40 @@ begin
 				r_reg360 + 1;			
 	
 	
-	ctrl <= (others=>'0') when en360hz = '1' AND ctrl = x"6" else					
-				(ctrl + 1) when en360hz = '1' AND ctrl < x"6" else 
-				ctrl; 
+	
+	ctrl_next <= (others=>'0') when en360hz = '1' AND ctrl = 6 else					
+				(ctrl + 1) when en360hz = '1' AND ctrl < 6 else 
+				ctrl;
 	
 	
-	tobcd <= SecU when ctrl = "000" else
-				SecT when ctrl = "001" else 
-				MinU when ctrl = "010" else
-				MinT when ctrl = "011" else
-				HourU when ctrl = "100" else
-				HourT when ctrl = "101" else
-				tobcd;
+	mux_tobcd: with ctrl select
+	tobcd <= SecU when "000",
+				SecT when "001",
+				MinU when "010",
+	 		   MinT when "011",
+				HourU when "100",
+				HourT when others;
+				
+	demux_tohex: with ctrl select
+			dout <= ((41 downto 7 => '0') & saidaconversor) when "000",
+					  ((41 downto 14 => '0') & saidaconversor & (6 downto 0 => '0')) when "001",
+						((41 downto 22  => '0') & saidaconversor & (14 downto 0 => '0')) when "010",
+						((41 downto 29=> '0') & saidaconversor & (21 downto 0 => '0')) when "011",
+						((41 downto 36 => '0') & saidaconversor & (28 downto 0 => '0')) when "100",
+						(saidaconversor & (34 downto 0 => '0') ) when others;
+						
+			HEX0 <= dout(6 downto 0);
+			HEX1 <= dout(13 downto 7);
+			HEX2 <= dout(20 downto 14);
+			HEX3 <= dout(27 downto 21);
+			HEX4 <= dout(34 downto 28);
+			HEX5 <= dout(41 downto 35);
  	
 	
 	-- output logic
 	en360hz <= '1' when r_reg360 = 28 else '0';
 	
 	en1hz <= '1' when r_reg = 9999 else '0'; --- enable em 1 hz
-	
-	
-
-	
 				 
 				 
     t0:   timer port map( clk   => CLOCK_10khz,
@@ -125,18 +147,23 @@ begin
 								  en => en1hz,
                           hourT => hourT );
 
-    bcd0: bcd2ssd port map (BCD => secU,
-	                        SSD => HEX0 );
-	bcd1: bcd2ssd port map (BCD => secT,
-	                        SSD => HEX1 );
-    bcd2: bcd2ssd port map (BCD => minU,
-	                        SSD => HEX2 );							
-	bcd3: bcd2ssd port map (BCD => minT,
-	                        SSD => HEX3 );						  
-	bcd4: bcd2ssd port map (BCD => hourU,
-	                        SSD => HEX4 );	
-    bcd5: bcd2ssd port map (BCD => hourT,
-	                        SSD => HEX5 );
+								  
+								  
+								  
+    bcd0: bcd2ssd port map (BCD => tobcd,
+	                        SSD => saidaconversor );
+									
+									
+--	bcd1: bcd2ssd port map (BCD => secT,
+--	                        SSD => HEX1 );
+--    bcd2: bcd2ssd port map (BCD => minU,
+--	                        SSD => HEX2 );							
+--	bcd3: bcd2ssd port map (BCD => minT,
+--	                        SSD => HEX3 );						  
+--	bcd4: bcd2ssd port map (BCD => hourU,
+--	                        SSD => HEX4 );	
+--    bcd5: bcd2ssd port map (BCD => hourT,
+--	                        SSD => HEX5 );
 							
 
 
